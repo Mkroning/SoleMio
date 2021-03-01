f909696f3a473247a768e7da481a3fd2891a36f5
Matheus Kroning <kroningmatheus@gmail.com>
2021-03-01 16:21:21 -0300

    :sparkles: first commit
    first commit

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+import reportWebVitals from './reportWebVitals';
+import GlobalStyle from './styles/globalStyles';
+import Frontpage from './pages/Frontpage';
+import theme from './styles/theme'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <title>{process.env.REACT_APP_PAGE_TITTLE}</title>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Frontpage />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
